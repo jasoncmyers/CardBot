46b396527a457e8ad7fccb1a6deb344c32c001ed
Jason Myers <myers.jason.c@gmail.com>
2020-03-27 19:56:24 -0500

    Small additions to card logic

--- cards.py
+++ cards.py
@@ -1,22 +1,23 @@
 import enum
 
 class Rank(enum.Enum):
-    two = ' 2'
-    three = ' 3'
-    four = ' 4'
-    five = ' 5'
-    six = ' 6'
-    seven = ' 7'
-    eight = ' 8'
-    nine = ' 9'
-    ten = '10'
-    jack = ' J'
-    queen = ' Q'
-    king = ' K'
-    ace = ' A'
+    two = (' 2', 2)
+    three = (' 3', 3)
+    four = (' 4', 4)
+    five = (' 5', 5)
+    six = (' 6', 6)
+    seven = (' 7', 7)
+    eight = (' 8', 8)
+    nine = (' 9', 9)
+    ten = ('10', 10)
+    jack = (' J', 11)
+    queen = (' Q', 12)
+    king = (' K', 13)
+    ace = (' A', 14)
+    joker = ('Joker', 99)
 
     def __str__(self):
-        return self.value
+        return self.value[0]
 
 class Suit(enum.Enum):
     spades = '♠️'
@@ -27,6 +28,14 @@ class Suit(enum.Enum):
     def __str__(self):
         return self.value
 
+class Card():
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return str(self.rank) + str(self.suit)
+
 class deck():
     def __init__(self):
         self.deck_type = 'standard'
@@ -35,8 +44,19 @@ class deck():
 
 
 if __name__ == "__main__":
-    print(Suit.spades)
+    #print (Rank.__members__)
+
+    c = Card(Rank.ace, Suit.clubs)
+    print(c)
+    
+    '''print(Suit.spades)
     print(Suit.spades.name)
     print(Rank.ace)
     print(Rank.ace.value)
     print(Suit)
+
+    for s in Suit:
+        print(s)
+    
+    for c in Rank[1:3]:
+        print(c)'''
